a2706c1666b97771f4eaafc212093248d2d5985e
Juanan <juanantonio.martinezl@um.es>
2022-06-30 16:35:52 +0200

    minor error fixed

--- app/config/database.py
+++ app/config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://juanan:password@localhost/brainlab"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://brainlab:password@localhost/brainlab"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
